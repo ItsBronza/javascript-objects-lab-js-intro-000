ed85f6d46d062011f2453e56aa2b4e857c4d81d8
Alyona <itsbronza@yahoo.com>
2019-11-07 09:21:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object[key]
+  delete object.key
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
 
-}
